218cf593b3d26362aa18ef6215d28691ca274073
JBMjese <6935@holbertonstudents.com>
2023-10-06 10:00:03 -0700

    test 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,15 +5,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
         result[i] = tuple_a[i] + tuple_b[i]
     return (result)
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    else:
-        return (len(sentence), sentence[0])
+        if sentence == "":
+            return (0, None)
+        else:
+            return (len(sentence), sentence[0])
 def max_integer(my_list=[]):
-    if not my_list:
-        return (None)
-    biggest_integer = my_list[0]
-    for integer in my_list[1:]:
-        if integer > biggest_integer:
-            biggest_integer = integer
-    return (biggest_integer)
+        if not my_list:
+            return (None)
+        biggest_integer = my_list[0]
+        for integer in my_list[1:]:
+            if integer > biggest_integer:
+                biggest_integer = integer
+        return (biggest_integer)
