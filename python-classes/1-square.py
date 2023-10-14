17eb31b19f8d7eba079998a17ce44e530ba109aa
JBMjese <135727452+JBMjese@users.noreply.github.com>
2023-10-14 18:26:50 -0500

    test

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
-
 class Square:
-    """
-    Square Class
-
-    A simple class to represent a square.
-
-    Attributes:
-        size (int): The size of the square.
-
-    Methods:
-        __init__(self, size): Initialize a Square object with a given size.
+    """"
+         This class represents a simple square.
     """
     def __init__(self, size):
-        """
-        Initialize a Square object with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
 
