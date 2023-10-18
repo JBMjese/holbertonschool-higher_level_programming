91bdb435efc07f47f0fdaf427df35f55ec1ef5c6
JBMjese <135727452+JBMjese@users.noreply.github.com>
2023-10-17 19:14:27 -0500

    test

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    This is an empty class representing a rectangle.
+
+    Attributes:
+        This class has no attributes.
+
+    Methods:
+        This class has no methods.
+    """
+    pass
