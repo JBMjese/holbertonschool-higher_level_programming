c31e9c6a1f782a4f0f4b8d581f20f07ef8fcb786
JBMjese <6935@holbertonstudents.com>
2024-02-15 11:20:29 -0500

    New test exercise

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,12 +4,12 @@
 if __name__ == "__main__":
     import sys
     """Print the number of and list of arguments."""
-    count = int(len(sys.argv) -1)
-    if count == 0:
-        print("{} arguments.".format(count))
-    elif count == 1:
-        print("{} argument:".format(count))
+    count = int(len(sys.argv))
+    if count == 1:
+        print("{} arguments.".format(count -1))
+    elif count == 2:
+        print("{} argument:".format(count -1))
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(count -1))
+    for i in range(1, count):
+        print("{}: {}".format(i, sys.argv[i]))
