abb78f62ac674f651761b105b61c118a4df677b0
JBMjese <6935@holbertonstudents.com>
2024-04-02 14:23:33 -0500

    :rocket: SQL statements

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Inserts a new row into the table first_table.
-mysql -e "USE $1; INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
