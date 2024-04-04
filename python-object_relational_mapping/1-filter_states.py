c91d465b7320a1bd492f162ae6323516108322bc
JBMjese <6935@holbertonstudents.com>
2024-04-04 11:01:26 -0500

    :sparkles: fix point 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
 
+    # By default, it will connect to localhost:3306
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
@@ -25,4 +26,4 @@ if __name__ == "__main__":
 
     for row in rows:
         print(row)
-        
+ 
