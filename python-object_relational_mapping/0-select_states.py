44146e8f041413dc2d143ce377796c008dca16ac
JBMjese <6935@holbertonstudents.com>
2024-04-04 10:39:41 -0500

    :sparkles: Point 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
 
-    # By default, it will connect to localhost:3306
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
