81de0f2d231e1a57ab57caf32bf6587870edcf6d
JBMjese <6935@holbertonstudents.com>
2024-04-05 11:41:25 -0500

    :green_heart: Point 13 and 14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+"""
+This module defines the City class that represents a table in the database.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    Represents the 'cities' table in the database.
+
+    Attributes:
+        id (int): The city's id.
+        name (str): (sqlalchemy.Integer): The city's name.
+        state_id (int): (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
