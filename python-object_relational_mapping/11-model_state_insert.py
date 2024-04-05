2020691369a48014e3558cc73e940877ad832132
JBMjese <6935@holbertonstudents.com>
2024-04-05 09:59:46 -0500

    :rocket: Point 10

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Prints the first State object from the database hbtn_0e_6_usa.
+ Usage: ./11-model_state_insert.py     <mysql username>
+                                       <mysql password>
+                                       <database name>
+                                       <state name searched>
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name="louisiana")
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
